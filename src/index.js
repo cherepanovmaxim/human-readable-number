88f9d2af998108cad8de9ac66705c85a16a6008c
cherepanovmaxim <42759942+cherepanovmaxim@users.noreply.github.com>
2021-03-04 19:41:07 -0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,112 @@
-module.exports = function toReadable (number) {
-  
-}
+const unit = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+];
+
+const des = [
+    "ten",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+];
+
+const overTen = [
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+];
+
+module.exports = function toReadable(num) {
+    if (num < 0 || num > 999) {
+        return "Number out of range";
+    } else if (typeof num !== "number") {
+        return "Numbers only";
+    } else if (num == 0) {
+        return "zero";
+    } else if (num > 10 && num < 20) {
+        return overTen[num.toString().charAt(1) - 1];
+    } else if (num.toString().length === 1 && num !== 0) {
+        return unit[num - 1];
+    } else if (num.toString().length === 2 && num % 10 === 0) {
+        return des[num.toString().charAt(0) - 1];
+    } else if (num.toString().length === 2 && num % 10 !== 0) {
+        return (
+            des[num.toString().charAt(0) - 1] +
+            " " +
+            unit[num.toString().charAt(1) - 1]
+        );
+    } else if (num.toString().length === 3 && num % 100 == 0) {
+        return unit[num.toString().charAt(0) - 1] + " " + "hundred";
+    } else if (
+        num.toString().length === 3 &&
+        num % 100 !== 0 &&
+        num.toString().charAt(1) == 0
+    ) {
+        return (
+            unit[num.toString().charAt(0) - 1] +
+            " " +
+            "hundred" +
+            " " +
+            unit[num.toString().charAt(2) - 1]
+        );
+    } else if (
+        num.toString().length === 3 &&
+        num % 100 !== 0 &&
+        num.toString().charAt(1) + num.toString().charAt(2) > 10 &&
+        num.toString().charAt(1) + num.toString().charAt(2) < 20
+    ) {
+        return (
+            unit[num.toString().charAt(0) - 1] +
+            " " +
+            "hundred" +
+            " " +
+            overTen[num.toString().charAt(2) - 1]
+        );
+    } else if (
+        num.toString().length === 3 &&
+        num % 100 !== 0 &&
+        (num.toString().charAt(1) + num.toString().charAt(2)) % 10 == 0
+    ) {
+        return (
+            unit[num.toString().charAt(0) - 1] +
+            " " +
+            "hundred" +
+            " " +
+            des[num.toString().charAt(1) - 1]
+        );
+    } else if (
+        num.toString().length === 3 &&
+        num % 100 !== 0 &&
+        (num.toString().charAt(1) + num.toString().charAt(2)) % 10 !== 0 &&
+        num.toString().charAt(1) !== 0
+    ) {
+        return (
+            unit[num.toString().charAt(0) - 1] +
+            " " +
+            "hundred" +
+            " " +
+            des[num.toString().charAt(1) - 1] +
+            " " +
+            unit[num.toString().charAt(2) - 1]
+        );
+    }
+};
+
